961f96cd204f04c17916248345b23de62e663964
unknown <rbalamgir@hotmail.com>
2020-10-16 23:08:11 +0600

    First commit

---
+++ LabManagementSystem.Shared/EquipmentCreationViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LabManagementSystem.Shared
+{
+    class EquipmentCreationViewModel
+    { 
+        [Required]
+        [Display(Name = "Equipment Name")]
+        public string Name { get; set; }
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+        [Required]
+        [Display(Name = "Submission Date")]
+        public DateTime SubmissionDate { get; set; }
+        [Required]
+        [Display(Name = "Status")]
+        public bool Status { get; set; }
+    }
+}
